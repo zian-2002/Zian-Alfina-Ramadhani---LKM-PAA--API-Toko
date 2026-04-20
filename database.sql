7e011aa9313025117d7c26d14e2345757de44976
zian-2002 <zianalfina223@gmail.com>
2026-04-20 22:34:34 +0700

    baru

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-﻿-- CUSTOMERS
+-- CUSTOMERS
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -32,3 +32,15 @@ CREATE TABLE orders (
 -- INDEX
 CREATE INDEX idx_customer ON orders(customer_id);
 CREATE INDEX idx_product ON orders(product_id);
+
+-- SAMPLE DATA (5)
+INSERT INTO customers (name, phone) VALUES
+('Zian','0811'),('Sinta','0822'),('Ofi','0833'),('Adel','0844'),('Lovya','0855');
+
+INSERT INTO products (name, price) VALUES
+('Shampoo',20000),('Pomade',50000),('Gel',30000),('Sisir',10000),('Spray',40000);
+
+INSERT INTO orders (customer_id, product_id, quantity, total_price) VALUES
+(1,1,2,40000),(2,2,1,50000),(3,3,3,90000),(4,4,2,20000),(5,5,1,40000);
+
+select * from products
